430fb71e03885aeec49dccf68b3feca81f2402f3
Muhammad Habib Rohman <mhrohman@live.com>
2017-05-17 22:22:08 +0700

    fix(build): github pages

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+process.env.NODE_ENV = 'production'
+
+const reactScriptsConfig = require('react-scripts-ts/config/webpack.config.prod')
+module.exports = Object.assign({}, reactScriptsConfig, {
+  output: Object.assign({}, reactScriptsConfig.output, {
+     publicPath: '/dimgos/'
+  })
+})
